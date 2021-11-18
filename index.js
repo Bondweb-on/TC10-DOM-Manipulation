d1ea7fb5122f31bf1eb8102209b9f2a8cd6fdf0c
Sebastián Gómez Morales <sebastian61910.sg@gmail.com>
2021-11-18 10:55:54 -0700

    Se añade el desarrollo del programa

---
+++ index.js
@@ -0,0 +1,53 @@
+const blueButton = document.getElementById("blue_button");
+const redButton = document.getElementById("red_button");
+const greenButton = document.getElementById("green_button");
+
+let coloredBox = document.getElementsByClassName("colored-box")[0];
+
+// Función anónima
+// function () {
+
+// }
+
+// Función flecha anónima
+// () => {
+
+// }
+
+// function foo() {
+
+// }
+
+// let bar = () => {
+
+// }
+
+let colors= ["blue", "red", "green"];
+
+function resetColor() {
+    for (const color of colors) {
+        coloredBox.classList.remove(color)    
+    }
+}
+
+function changeColorToBlue() {
+    resetColor();
+    coloredBox.classList.add("blue")
+}
+
+
+function changeColorToRed() {
+    resetColor();
+    coloredBox.classList.add("red")
+}
+
+
+function changeColorToGreen() {
+    resetColor();
+    coloredBox.classList.add("green")
+}
+
+blueButton.addEventListener("click", changeColorToBlue);
+redButton.addEventListener("click", changeColorToRed);
+greenButton.addEventListener("click", changeColorToGreen);
+
